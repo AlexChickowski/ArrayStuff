1af6fbddb552575b64c3b281e602db2ca9b27381
AlexChickowski <71711669+AlexChickowski@users.noreply.github.com>
2020-11-02 09:10:04 -0500

    erased TODO

--- ArrayStuff.java
+++ ArrayStuff.java
@@ -14,17 +14,17 @@ public class ArrayStuff {
         
         // PART A
 
-        // TODO: Create integer array called "ages" with length 5.
+        // Create integer array called "ages" with length 5.
         int[] ages = new int[5];
 
-        // TODO: Change the 3rd element of "ages" to be 18
+        // Change the 3rd element of "ages" to be 18
         ages[2] = 18;
 
-        // TODO: Change the first element of "ages" to be an integer entered by the user
+        // Change the first element of "ages" to be an integer entered by the user
         System.err.println("Enter an integer:");
         ages[0] = input.nextInt();
 
-        // TODO: Change the last element of "ages" to be the first element squared
+        // Change the last element of "ages" to be the first element squared
         ages[4] = ages[0] * ages[0];
 
         // TODO: Print all of the elements of "ages", one per line
@@ -40,28 +40,28 @@ public class ArrayStuff {
         // names of the planets in order from closest to the sun to farthest.
         // NOTE: Pluto is NOT a planet.
         String[] planets = {
-        		"mercury",
-        		"venus",
-        		"earth",
-        		"mars",
-        		"jupiter",
-        		"saturn",
-        		"uranus",
+        		"Mercury",
+        		"Venus",
+        		"Earth",
+        		"Mars",
+        		"Jupiter",
+        		"Saturn",
+        		"Uranus",
         		"neptune"
         };
         
-        // TODO: Print the first letter of the 7th planet from the sun        
+        // Print the first letter of the 7th planet from the sun        
         System.out.println(planets[6].charAt(0));
         
-        // TODO: Change the name of our planet to be a word that the user inputs
+        // Change the name of our planet to be a word that the user inputs
         System.out.println("What do you want to rename Earth to be?");
         planets[2] = input.next();
 		
-        // TODO: Replace the first planet with the planet at index 2, but with all
+        // Replace the first planet with the planet at index 2, but with all
         // upper case letters.
         planets[0] = planets[2].toUpperCase();
 
-        // TODO: Print all of the elements of "planets" in reverse order.
+        // Print all of the elements of "planets" in reverse order.
         for (int count = planets.length - 1; count >= 0; count--) {
         	System.out.println(planets[count]);
         }
